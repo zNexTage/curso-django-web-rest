214c7ff93d63b7bc144cd5520a0713eb297afaa6
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-06-29 22:06:56 -0300

    Utilização de templates

--- primeiro-projeto/recipes/views.py
+++ primeiro-projeto/recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home 123')
+    return render(request, 'home.html')
     #return HTTPResponse
 
 def contact(request):
