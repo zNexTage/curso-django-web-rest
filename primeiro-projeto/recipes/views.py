18db07f192cabfa41a4edce6a5f81089fca1b712
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-07-28 20:26:43 -0300

    Utilização de dados falsos para montar a páginas de receitas.

--- primeiro-projeto/recipes/views.py
+++ primeiro-projeto/recipes/views.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.data.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Christian'
+        'recipes': [make_recipe() for _ in range(10)]
     })
     #return HTTPResponse
 
 
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe_view.html', context={
-        'name': 'Christian'
+        'recipe': make_recipe()
     })
 
 
