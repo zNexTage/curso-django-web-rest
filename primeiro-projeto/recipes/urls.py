3efb4c66caba3bce9a00d655e29b284febf8fe84
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-07-28 21:11:37 -0300

    Utilização de names para as urls.

--- primeiro-projeto/recipes/urls.py
+++ primeiro-projeto/recipes/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     #Path -> Rota, View
-    path('', views.home), # Home
-    path('<int:id>/', views.recipes)
+    path('', views.home, name='home'), # Home
+    path('<int:id>/', views.recipes, name='recipe')
     # path('sobre/', about), # /sobre/
     # path('contato/', contact), # /contato/
 ]
