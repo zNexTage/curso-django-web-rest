a8cccb97a294299c94f01c31f8558b3cb71a4b33
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-09-29 20:39:00 -0300

    Adicionei a pasta media na urls.

--- primeiro-projeto/projeto/urls.py
+++ primeiro-projeto/projeto/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #Path -> Rota, View
+    # Path -> Rota, View
     path('recipes/', include("recipes.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
