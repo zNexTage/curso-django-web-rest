a7687dda337f6229db831227969487f1806ab709
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-30 17:45:05 -0300

    rotas e views para receitas e categorias.

--- primeiro-projeto/recipes/urls.py
+++ primeiro-projeto/recipes/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    #Path -> Rota, View
-    path('', views.home, name='home'), # Home
-    path('<int:id>/', views.recipes, name='recipe')
+    # Path -> Rota, View
+    path('', views.home, name='home'),  # Home
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
+    path('recipes/<int:id>/', views.recipes, name='recipe')
     # path('sobre/', about), # /sobre/
     # path('contato/', contact), # /contato/
 ]
--- primeiro-projeto/recipes/views.py
+++ primeiro-projeto/recipes/views.py
@@ -2,13 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.data.factory import make_recipe
 
+from recipes.models import Recipe
+
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
     })
-    #return HTTPResponse
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__pk=category_id).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
+    })
+    # return HTTPResponse
 
 
 def recipes(request, id):
